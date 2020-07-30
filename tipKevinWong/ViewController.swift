18cdec770f014fc104e4935c0d7ebc7fdf39ba4d
Kevin Wong <kew005@ucsd.edu>
2020-07-30 14:43:26 -0700

    Initial Commit

---
+++ tipKevinWong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipKevinWong
+//
+//  Created by Kevin Wong on 7/30/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
